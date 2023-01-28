f44fc0e6397abc4b7eff760a8f147b275baf1bb4
Maxwell Stone <maxwell@kinetixml.com>
2023-01-28 11:41:20 -0500

    changes

---
+++ whisper/main.py
@@ -0,0 +1,23 @@
+import functions_framework
+import whisper as wspr
+
+@functions_framework.http
+def whisper(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    Note:
+        For more information on how Flask integrates with Cloud
+        Functions, see the `Writing HTTP functions` page.
+        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
+    """
+    try:
+        model = wspr.load_model("base")
+        return "Model Loaded"
+    except Exception as e:
+        return e
