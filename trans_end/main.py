a39239677fa49b082842d862131a8a39c7dc5f23
Maxwell Stone <maxwell@kinetixml.com>
2023-01-29 03:20:39 -0500

    add trans_end

---
+++ trans_end/main.py
@@ -0,0 +1,36 @@
+import functions_framework
+from urllib.request import urlopen
+import json
+import requests
+
+
+# Register a CloudEvent function with the Functions Framework
+@functions_framework.cloud_event
+def trans_end(cloud_event):
+    # Your code here
+    # Access the CloudEvent data payload via cloud_event.data
+    #print(f"Cloud_Event Data: {str(cloud_event.data)}")
+    print(cloud_event)
+    url = cloud_event.data["mediaLink"]
+    id = cloud_event.data["name"].split(".")[0]
+    
+    # store the response of URL
+    response = urlopen(url)
+    
+    # storing the JSON response 
+    # from url in data
+    data_json = json.loads(response.read())
+
+    transcription = ""
+    for result in data_json["results"]:
+        transcription = transcription + " " + result["alternatives"][0]["transcript"]
+
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    data = {
+        "id": id,
+        "transcription": transcription
+    }
+    
+    callback = "https://hkwrlworzfpsgkaxcobm.functions.supabase.co/transcription_callback"
+    response = requests.post(callback, headers=headers, json=data)
+    return "Hello World"
